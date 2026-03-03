78fbe78efd54993b2c3d529a872d4b8dd6349add
kajalpatel222 <kajal.software.engineer.222@gmail.com>
2026-03-02 16:19:49 -0800

    Add findMissingNumberInSequence function
    Implement function to find missing number in sequence

--- HashingOrCounting/findMissingNumberInSequence.js
+++ HashingOrCounting/findMissingNumberInSequence.js
@@ -1,1 +1,21 @@
 // findMissingNumberInSequence.js
+/**
+ * @param {number[]} numbers
+ * @return {number}
+ */
+function findMissingNumberInSequence(numbers) {
+  let actualSum = 0;
+  let expectedSum = 0;
+
+  for (let num of numbers) {
+    actualSum += num;
+  }
+
+  for (let i = 0; i <= numbers.length; i++) {
+    expectedSum += i;
+  }
+
+  return expectedSum - actualSum;
+}
+
+console.log(findMissingNumberInSequence([0, 1, 3, 4, 5])) // 2
